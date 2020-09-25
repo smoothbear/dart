6f5503d58d6ee04fddae19ca33e20bdb93cca350
kjbin0420 <kjbin0420@naver.com>
2020-09-25 17:08:13 +0900

    Flutter App with Google Maps API

--- Day 2/google_map/ios/Runner/AppDelegate.swift
+++ Day 2/google_map/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,8 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
--- Day 2/google_map/lib/main.dart
+++ Day 2/google_map/lib/main.dart
@@ -11,17 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Google Maps',
+      home: MapsState(),
     );
   }
 }
 
 class MapsState extends StatefulWidget {
   @override
-  _My
+  _MyMapsPage createState() => _MyMapsPage();
 }
 
 class _MyMapsPage extends State<MapsState> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final daeMaGo = CameraPosition(target: LatLng(36.3917, 127.3631), zoom: 3.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition: daeMaGo,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
 }
