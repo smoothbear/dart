8be87b40f61d68820e46537e9b0502032eee8ee6
kjbin0420 <kjbin0420@naver.com>
2020-10-03 16:04:11 +0900

    added Stateful widget

--- Note app/Note_app/lib/main.dart
+++ Note app/Note_app/lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 void main() => runApp();
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth =
+}
